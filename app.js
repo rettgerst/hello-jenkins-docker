e7af6f38d8fe49a023c1f7213f7cba3ca8c35abd
rett gerst <tricagamma@gmail.com>
2019-03-15 11:39:36 -0400

    add a new route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
 	res.send('hello again, again jenkins!')
 });
 
+app.get('/randomnumber', (req, res) => {
+	res.send(Math.random());
+});
+
 const port = process.env.SERVER_PORT || 8080;
 
 app.listen(port, () => console.log('listening on', port));
